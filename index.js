4afd09b0f5498fd83fe38ea40528907413d7c6f6
Otabek <otabek.akbaroff@gmail.com>
2020-03-01 13:41:02 -0500

    basic stuff, testing API

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+
+const server=express();
+
+server.use(express.json());
+
+server.get('/', (req,res)=>{
+    const test=[{test:true, newTestRan:'yes'}]
+
+    res.status(200).json(test);
+})
+
+
+const port=5000;
+server.listen(port,()=> console.log(`\n*** Running on port ${port}`))
