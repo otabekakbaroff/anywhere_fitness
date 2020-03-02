5742cd7540ca725769afb26cf55ea432ca393f64
Otabek <otabek.akbaroff@gmail.com>
2020-03-02 10:37:59 -0500

    wa

--- seeds/003-classes.js
+++ seeds/003-classes.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('class').truncate()
+  return knex('class').del()
     .then(function () {
       // Inserts seed entries
       return knex('class').insert([
--- seeds/004-clients.js
+++ seeds/004-clients.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('client').truncate()
+  return knex('client').del()
     .then(function () {
       // Inserts seed entries
       return knex('client').insert([
