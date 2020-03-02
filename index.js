d61f856aecb92acfd863299e52e755e96a3ec230
Otabek <otabek.akbaroff@gmail.com>
2020-03-02 09:52:44 -0500

    NEWPort

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server.js');
 
 
-const port=process.env.PORT || 5000;
+const port=process.env.PORT || 5432;
 server.listen(port,()=> console.log(`\n*** Running on port ${port}`))
