031de9f9958539e9212e4e4e03766b53283074d7
Otabek <otabek.akbaroff@gmail.com>
2020-03-01 20:35:38 -0500

    ReT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // const Instructor = require('./users/instructor-router');
 
-// const  auth_router=require('./auth/auth-router');
+const  auth_router=require('./auth/auth-router');
 
 // const Instructor=require('./users/instructor/instructor-router');
 
