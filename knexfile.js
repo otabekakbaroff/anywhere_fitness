aa0400b71648105963e2fa9e0c962c6d9fd8bc33
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-09-12 18:58:12 -0400

    still testing

--- knexfile.js
+++ knexfile.js
@@ -25,14 +25,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './migrations'
     }
   }
 };
